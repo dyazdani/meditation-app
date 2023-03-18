744630ece80ee79630b3fa171ebdde0b175c51aa
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-03-18 12:07:59 -0700

    Adding function for setting audio

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,16 @@ const header = document.querySelector('h1');
 const select = document.querySelector('select');
 const audio = document.querySelector('audio');
 
+// ------* function declarations *---------
+
+function setAudio() {
+    if (event.target.value === "beach") {
+        audio.setAttribute('src', '../audio.beach.mp3');
+    } else if (event.target.value === "forest") {
+        audio.setAttribute('src', '../audio.forest.mp3');
+    } else if (event.target.value === "rain") {
+        audio.setAttribute('src', '../audio.rain.mp3');
+    } else if (event.target.value === "stream") {
+        audio.setAttribute('src', '../audio.stream.mp3');
+    }
+}

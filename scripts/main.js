46382d10128d607eff896e4a73f2707399939d9b
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-03-18 14:47:52 -0700

    Added function for select element event listener to change localStorage object

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,13 @@ const audio = document.querySelector('audio');
 
 // ------* function declarations *---------
 
-function setAudio() {
-    if (event.target.value === "beach") {
-        audio.setAttribute('src', '../audio.beach.mp3');
-    } else if (event.target.value === "forest") {
-        audio.setAttribute('src', '../audio.forest.mp3');
-    } else if (event.target.value === "rain") {
-        audio.setAttribute('src', '../audio.rain.mp3');
-    } else if (event.target.value === "stream") {
-        audio.setAttribute('src', '../audio.stream.mp3');
-    }
+function onScreenOptionSelected(event) {
+    const screenSelection = event.target.value;
+    localStorage.setItem('selection', screenSelection);
+    const selection = localStorage.getItem('selection');
+
+    
+    renderBackground();
+    renderSelectionBackground();
+    renderAudio();
 }

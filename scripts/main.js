7cf38bfa5c52348caec28d26e12708448b18811f
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-03-18 12:02:43 -0700

    Selecting elements taht will need to change with each screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
-// TODO: add JS
+// ------* selecting elements that will change with screen selection *--------
+const header = document.querySelector('h1');
+const select = document.querySelector('select');
+const audio = document.querySelector('audio');
+
